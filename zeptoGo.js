2a98474b23d4297fbc19e24467acecb6c6793797
Lake1GitHub <1695064014@qq.com>
2017-11-07 18:08:15 +0800

    update

---
+++ zeptoGo.js
@@ -0,0 +1,15 @@
+(function(global, factory){
+    if(typeof define === 'function' && define.amd)
+        define(function(){ return factory(global) })
+    else
+        factory(gloal)
+}(this, function(window){
+    var Zepto = (function(){
+        var undefined, key, $, classList, emptyArray = [], concat = emptyArray.concat, filter = emptyArray.filter, slice = emptyArray.slice,
+            document = window.document,
+            elementDisplay = {}, classCache = {},
+            cssNumber = { 'column-count': 1, 'columns': 1, 'font-weight': 1, 'line-height': 1, 'opacity': 1, 'z-index': 1, 'zoom': 1},
+            fragmentRE = /^\s*<(\w+|!)[^>]*>/,
+            
+    })
+})
